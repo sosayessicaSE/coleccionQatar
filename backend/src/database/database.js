b3e24803fe6ed363029b1589c372af98366b3f60
sdvenus <dahiana-s@hotmail.com>
2024-06-24 19:46:52 -0300

    Second commit

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize("figuritas", "", "", {
+export const sequelize = new Sequelize("figuritas", "root", "y.d.s.m03", {
   host: "localhost",
   dialect: "mysql",
 });
